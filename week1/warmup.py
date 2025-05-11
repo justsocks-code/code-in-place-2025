edee1984ec184859cb5fdb442387120d553674e6
Rumyana Baldzhieva <80819920+justsocks-code@users.noreply.github.com>
2025-05-11 22:35:05 +0300

    Create warmup.py

---
+++ week1/warmup.py
@@ -0,0 +1,16 @@
+from karel.stanfordkarel import *
+
+# File: warmup.py
+# -----------------------------
+# Challenge: Warmup
+# Karel should move forward, pick up a beeper,
+# then move forward again.
+
+def main():
+    move()
+    pick_beeper()
+    move()
+
+# Don't edit these next lines — they make sure main() runs.
+if __name__ == '__main__':
+    main()
