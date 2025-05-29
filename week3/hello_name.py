5f295fe510b7a83f7ea5338539ac74b981921c21
Rumyana Baldzhieva <80819920+justsocks-code@users.noreply.github.com>
2025-05-29 18:23:33 +0300

    Create hello_name.py

---
+++ week3/hello_name.py
@@ -0,0 +1,11 @@
+# File: hello_name.py
+# -----------------------------
+# A basic Python program that asks the user for their name
+# and prints a personalized greeting.
+
+def main():
+    result = input("What is your name? ")
+    print("Hello", result)
+
+if __name__ == '__main__':
+    main()
