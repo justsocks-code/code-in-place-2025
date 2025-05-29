a246c14bb8d4eb09cc5f29d00c499a5b7659a9bb
Rumyana Baldzhieva <80819920+justsocks-code@users.noreply.github.com>
2025-05-29 18:25:04 +0300

    Update sunrise_fill_in.py

--- week3/sunrise_fill_in.py
+++ week3/sunrise_fill_in.py
@@ -9,8 +9,7 @@ def main():
     adjective = input("An adjective: ")
     goal = input("A goal you would like to achieve: ")
     
-    print(f"At dawn the sky turned {color}, and the air felt {adjective}.")
-    print(f"I decided today I will finally {goal}.")
+    print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.")
 
 if __name__ == "__main__":
     main()
