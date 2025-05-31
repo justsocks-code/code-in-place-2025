116adb8785c5a059109a1fd53bc9d8cbe045578b
Rumyana Baldzhieva <80819920+justsocks-code@users.noreply.github.com>
2025-06-01 01:11:30 +0300

    Create liftoff.py

---
+++ week4/liftoff.py
@@ -0,0 +1,12 @@
+# File: liftoff.py
+# -----------------------------
+# Counts down from 10 to 1 and prints "Liftoff!" at the end.
+
+def main():
+    for i in range(10):
+        print(10 - i)
+    print("Liftoff!")
+
+# Required line to run main
+if __name__ == '__main__':
+    main()
